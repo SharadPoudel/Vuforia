8c5a3df5dd4b08e7b85e78ddbbc019d6d7a86d7d
SharadPoudel <sharadpoudel.info@gmail.com>
2023-02-14 23:19:19 +0100

    MultiImageTarget achieved

---
+++ My project/AR_ MultIamge_track_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,89 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void MultiTargetManager::Start()
+extern void MultiTargetManager_Start_mD151C5E0A45B9B43F1DB8CFF4F31E5B25328ECBA (void);
+// 0x00000002 System.Void MultiTargetManager::OnEnable()
+extern void MultiTargetManager_OnEnable_m88669D2707D14D66EF795B799B171CDD6D1E5ADF (void);
+// 0x00000003 System.Void MultiTargetManager::OnDisable()
+extern void MultiTargetManager_OnDisable_mDB86FAC75D1E9D67B17EEA068688CA097B830F65 (void);
+// 0x00000004 System.Void MultiTargetManager::ImageFound(UnityEngine.XR.ARFoundation.ARTrackedImagesChangedEventArgs)
+extern void MultiTargetManager_ImageFound_mF3CE26814931A40891B6B3E066635374B270004B (void);
+// 0x00000005 System.Void MultiTargetManager::ShowModel(UnityEngine.XR.ARFoundation.ARTrackedImage)
+extern void MultiTargetManager_ShowModel_mFE67CBC679A37940AB90600D03CC2432853389D6 (void);
+// 0x00000006 System.Void MultiTargetManager::HideARModel(UnityEngine.XR.ARFoundation.ARTrackedImage)
+extern void MultiTargetManager_HideARModel_m5DB137018E45CB9D91A5BCB4AE865353853024B6 (void);
+// 0x00000007 System.Void MultiTargetManager::.ctor()
+extern void MultiTargetManager__ctor_mCF461ED7AC14DCEF98BF2E11D0F76173769DF4F2 (void);
+// 0x00000008 System.Void PlanetRing::OnEnable()
+extern void PlanetRing_OnEnable_mD777DB0E4EBEE5C944BE60E1F309137AFA18B6AA (void);
+// 0x00000009 System.Void PlanetRing::OnValidate()
+extern void PlanetRing_OnValidate_m06C3E8A17B5698B322C4FB541E0F4E0E83942E3A (void);
+// 0x0000000A System.Void PlanetRing::SetUpRing()
+extern void PlanetRing_SetUpRing_m0C67476ECF054A32E49AF1DF0672CB0274C8A804 (void);
+// 0x0000000B System.Void PlanetRing::BuildRingMesh()
+extern void PlanetRing_BuildRingMesh_m8B30D1CCCA913798A181C4EE1D5D1F78A4C94A43 (void);
+// 0x0000000C System.Void PlanetRing::.ctor()
+extern void PlanetRing__ctor_m588D8856EC9E44FC4621B2AB29C57BB2E91B2042 (void);
+static Il2CppMethodPointer s_methodPointers[12] = 
+{
+	MultiTargetManager_Start_mD151C5E0A45B9B43F1DB8CFF4F31E5B25328ECBA,
+	MultiTargetManager_OnEnable_m88669D2707D14D66EF795B799B171CDD6D1E5ADF,
+	MultiTargetManager_OnDisable_mDB86FAC75D1E9D67B17EEA068688CA097B830F65,
+	MultiTargetManager_ImageFound_mF3CE26814931A40891B6B3E066635374B270004B,
+	MultiTargetManager_ShowModel_mFE67CBC679A37940AB90600D03CC2432853389D6,
+	MultiTargetManager_HideARModel_m5DB137018E45CB9D91A5BCB4AE865353853024B6,
+	MultiTargetManager__ctor_mCF461ED7AC14DCEF98BF2E11D0F76173769DF4F2,
+	PlanetRing_OnEnable_mD777DB0E4EBEE5C944BE60E1F309137AFA18B6AA,
+	PlanetRing_OnValidate_m06C3E8A17B5698B322C4FB541E0F4E0E83942E3A,
+	PlanetRing_SetUpRing_m0C67476ECF054A32E49AF1DF0672CB0274C8A804,
+	PlanetRing_BuildRingMesh_m8B30D1CCCA913798A181C4EE1D5D1F78A4C94A43,
+	PlanetRing__ctor_m588D8856EC9E44FC4621B2AB29C57BB2E91B2042,
+};
+static const int32_t s_InvokerIndices[12] = 
+{
+	5868,
+	5868,
+	5868,
+	4555,
+	4644,
+	4644,
+	5868,
+	5868,
+	5868,
+	5868,
+	5868,
+	5868,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	12,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
