f79601da257a79dbef8d84ee1daf7fe502896e4a
SharadPoudel <sharadpoudel.info@gmail.com>
2023-02-14 16:03:33 +0100

    Update from TUF gaming Laptop

---
+++ My project/ARtest_4_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,117 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void HighlightNearestFace::Start()
+extern void HighlightNearestFace_Start_mF28EDF79E2E47721A575C97C6E9B1043D7080B8D (void);
+// 0x00000002 System.Void HighlightNearestFace::Update()
+extern void HighlightNearestFace_Update_m4DB3C468AC335189002E2112ABAF49FACD6603D1 (void);
+// 0x00000003 UnityEngine.Vector3 HighlightNearestFace::FaceCenter(pb_Object,ProBuilder2.Common.pb_Face)
+extern void HighlightNearestFace_FaceCenter_mE314CC4F157712E6827EF7FBC24D0D06309E1E50 (void);
+// 0x00000004 System.Void HighlightNearestFace::.ctor()
+extern void HighlightNearestFace__ctor_mA13104CA324FAE27D381B32B26CA562C846ED786 (void);
+// 0x00000005 System.Void MultiTargetManager::Start()
+extern void MultiTargetManager_Start_mD151C5E0A45B9B43F1DB8CFF4F31E5B25328ECBA (void);
+// 0x00000006 System.Void MultiTargetManager::OnEnable()
+extern void MultiTargetManager_OnEnable_m88669D2707D14D66EF795B799B171CDD6D1E5ADF (void);
+// 0x00000007 System.Void MultiTargetManager::OnDisable()
+extern void MultiTargetManager_OnDisable_mDB86FAC75D1E9D67B17EEA068688CA097B830F65 (void);
+// 0x00000008 System.Void MultiTargetManager::ImageFound(UnityEngine.XR.ARFoundation.ARTrackedImagesChangedEventArgs)
+extern void MultiTargetManager_ImageFound_mF3CE26814931A40891B6B3E066635374B270004B (void);
+// 0x00000009 System.Void MultiTargetManager::ShowModel(UnityEngine.XR.ARFoundation.ARTrackedImage)
+extern void MultiTargetManager_ShowModel_mFE67CBC679A37940AB90600D03CC2432853389D6 (void);
+// 0x0000000A System.Void MultiTargetManager::HideARModel(UnityEngine.XR.ARFoundation.ARTrackedImage)
+extern void MultiTargetManager_HideARModel_m5DB137018E45CB9D91A5BCB4AE865353853024B6 (void);
+// 0x0000000B System.Void MultiTargetManager::.ctor()
+extern void MultiTargetManager__ctor_mCF461ED7AC14DCEF98BF2E11D0F76173769DF4F2 (void);
+// 0x0000000C System.Void PlanetRing::OnEnable()
+extern void PlanetRing_OnEnable_mD777DB0E4EBEE5C944BE60E1F309137AFA18B6AA (void);
+// 0x0000000D System.Void PlanetRing::OnValidate()
+extern void PlanetRing_OnValidate_m06C3E8A17B5698B322C4FB541E0F4E0E83942E3A (void);
+// 0x0000000E System.Void PlanetRing::SetUpRing()
+extern void PlanetRing_SetUpRing_m0C67476ECF054A32E49AF1DF0672CB0274C8A804 (void);
+// 0x0000000F System.Void PlanetRing::BuildRingMesh()
+extern void PlanetRing_BuildRingMesh_m8B30D1CCCA913798A181C4EE1D5D1F78A4C94A43 (void);
+// 0x00000010 System.Void PlanetRing::.ctor()
+extern void PlanetRing__ctor_m588D8856EC9E44FC4621B2AB29C57BB2E91B2042 (void);
+// 0x00000011 System.Void ProBuilder2.Examples.CameraControls::Start()
+extern void CameraControls_Start_m1F9D32AC316EA56C18261A551BAE54FA847E11EB (void);
+// 0x00000012 System.Void ProBuilder2.Examples.CameraControls::LateUpdate()
+extern void CameraControls_LateUpdate_m44E6711A090286CC78285012586913F4AEF354C8 (void);
+// 0x00000013 System.Void ProBuilder2.Examples.CameraControls::.ctor()
+extern void CameraControls__ctor_mAC68AA961C873446B855B25ADC59A920D56CC5BC (void);
+static Il2CppMethodPointer s_methodPointers[19] = 
+{
+	HighlightNearestFace_Start_mF28EDF79E2E47721A575C97C6E9B1043D7080B8D,
+	HighlightNearestFace_Update_m4DB3C468AC335189002E2112ABAF49FACD6603D1,
+	HighlightNearestFace_FaceCenter_mE314CC4F157712E6827EF7FBC24D0D06309E1E50,
+	HighlightNearestFace__ctor_mA13104CA324FAE27D381B32B26CA562C846ED786,
+	MultiTargetManager_Start_mD151C5E0A45B9B43F1DB8CFF4F31E5B25328ECBA,
+	MultiTargetManager_OnEnable_m88669D2707D14D66EF795B799B171CDD6D1E5ADF,
+	MultiTargetManager_OnDisable_mDB86FAC75D1E9D67B17EEA068688CA097B830F65,
+	MultiTargetManager_ImageFound_mF3CE26814931A40891B6B3E066635374B270004B,
+	MultiTargetManager_ShowModel_mFE67CBC679A37940AB90600D03CC2432853389D6,
+	MultiTargetManager_HideARModel_m5DB137018E45CB9D91A5BCB4AE865353853024B6,
+	MultiTargetManager__ctor_mCF461ED7AC14DCEF98BF2E11D0F76173769DF4F2,
+	PlanetRing_OnEnable_mD777DB0E4EBEE5C944BE60E1F309137AFA18B6AA,
+	PlanetRing_OnValidate_m06C3E8A17B5698B322C4FB541E0F4E0E83942E3A,
+	PlanetRing_SetUpRing_m0C67476ECF054A32E49AF1DF0672CB0274C8A804,
+	PlanetRing_BuildRingMesh_m8B30D1CCCA913798A181C4EE1D5D1F78A4C94A43,
+	PlanetRing__ctor_m588D8856EC9E44FC4621B2AB29C57BB2E91B2042,
+	CameraControls_Start_m1F9D32AC316EA56C18261A551BAE54FA847E11EB,
+	CameraControls_LateUpdate_m44E6711A090286CC78285012586913F4AEF354C8,
+	CameraControls__ctor_mAC68AA961C873446B855B25ADC59A920D56CC5BC,
+};
+static const int32_t s_InvokerIndices[19] = 
+{
+	6049,
+	6049,
+	2073,
+	6049,
+	6049,
+	6049,
+	6049,
+	4690,
+	4779,
+	4779,
+	6049,
+	6049,
+	6049,
+	6049,
+	6049,
+	6049,
+	6049,
+	6049,
+	6049,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	19,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
---
+++ My project/Assets/Script/MultiTargetManager.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class MultiTargetManager : MonoBehaviour
+{
+
+    [SerializeField] private ARTrackedImageManager aRTrackedImageManager;
+    [SerializeField] private GameObject[] aRModelsToCreate;//creating Array
+
+    //creating dictionaries
+    private Dictionary<string, GameObject> aRModels = new Dictionary<string, GameObject>();
+    private Dictionary<string, bool> modelState = new Dictionary<string, bool>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach ( var arModel in aRModelsToCreate)
+            {
+            GameObject newARModel = Instantiate(arModel, Vector3.zero, Quaternion.identity);
+            newARModel.name = arModel.name;
+            aRModels.Add(newARModel.name, newARModel);
+            newARModel.SetActive(false);
+            modelState.Add(newARModel.name, false);
+
+
+            }
+        
+    }
+
+    private void OnEnable()
+        {
+        aRTrackedImageManager.trackedImagesChanged += ImageFound;
+
+        }
+
+    private void OnDisable()
+        {
+        aRTrackedImageManager.trackedImagesChanged -= ImageFound;
+        }
+
+    private void ImageFound(ARTrackedImagesChangedEventArgs eventData)
+        {
+            foreach(var trackedImage in eventData.added)
+            {
+
+            }
+
+            foreach( var trackedImage in eventData.updated)
+            {
+            if (trackedImage.trackingState == TrackingState.Tracking)
+
+                {
+                ShowModel(trackedImage);
+                }
+            else if(trackedImage.trackingState== TrackingState.Limited)
+                {
+                HideARModel(trackedImage);
+                }
+            }
+
+        }
+    private void ShowModel(ARTrackedImage trackedImage)
+        {
+        bool isModelActivated = modelState[trackedImage.referenceImage.name];
+        if (!isModelActivated)
+            {
+            GameObject aRModel = aRModels[trackedImage.referenceImage.name];
+            aRModel.transform.position = trackedImage.transform.position;
+            aRModel.SetActive(true);
+            modelState[trackedImage.referenceImage.name] = true;
+            }
+        else
+            {
+            GameObject aRModel = aRModels[trackedImage.referenceImage.name];
+            aRModel.transform.position = trackedImage.transform.position;
+            }
+        }
+
+    private void HideARModel(ARTrackedImage trackedImage)
+        {
+        bool isModelActivated = modelState[trackedImage.referenceImage.name];
+        if (isModelActivated)
+            {
+            GameObject aRModel = aRModels[trackedImage.referenceImage.name];
+           // aRModel.transform.position = trackedImage.transform.position;
+            aRModel.SetActive(false);
+            modelState[trackedImage.referenceImage.name] = false;
+            }
+        }
+
+
+        
+    }
